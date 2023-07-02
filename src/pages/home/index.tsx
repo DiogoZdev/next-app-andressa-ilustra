2366b821d6c6dac8ac361c4da4b76add2bdc8a1c
Diogo <diogozang2@gmail.com>
2023-07-01 23:07:16 -0300

    fix: small fix
    small fix

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,21 +1,8 @@
 import { Header } from "@/components/layout/Header";
 import { Gallery }  from "@/components/compositions/Gallery";
-
-import { GetStaticProps } from "next";
-import { ProjectsList } from "@/interfaces/project.interface";
 import { Contact } from "@/components/layout/Contact";
 
-export const getStaticProps: GetStaticProps = async () => {
-  const { projects } = await fetch(
-    "https://raw.githubusercontent.com/andressadesign/files/main/projects.json"
-  ).then((res) => res.json() as Promise<ProjectsList>);
-
-  console.log(projects)
-
-  return { props: { projects } }
-}
-
-export const Home = () => {
+export default function Home() {
   return (
     <>
       <Header />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Home } from "./home";
+import Home from "./home";
 
 export default function Index() {
   return <Home/>;
