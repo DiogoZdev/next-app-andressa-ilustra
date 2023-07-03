dbacde762c6a274abc25bbf43a97737d80173b6b
Diogo <diogozang2@gmail.com>
2023-07-03 20:56:55 -0300

    feat: add favicon and title
    add favicon and title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,16 @@ import { Footer } from "@/components/layout/Footer";
 import { Nav } from "@/components/layout/Navigation";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <>
+    <Head>
+      <link rel="icon" href="/logo-feather.svg" sizes="32x32, 64x64, 128x128" />
+      <title>Andressa Araujo</title>
+    </Head>
     <Nav/>
     <Component {...pageProps} />
     <Footer />
